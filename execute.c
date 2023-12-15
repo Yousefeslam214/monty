621a150e15161aae11a3f05f97739dbbe8e78a98
Yousef eslam <yousefeslam214@gmail.com>
2023-12-16 00:16:21 +0200

    done

---
+++ execute.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+void execute(Data *data)
+{
+	int i = 0;
+	char *str, *op;
+
+	instruction_t ops[] = {
+		{"push", push},{"pall", pall}
+	};
+	op = strtok(data->line, " \n\t");
+	str = strtok(NULL, " \n\t");
+	if (op == NULL)
+		return;
+	else if (op[0] == '#')
+		return;
+	while (ops[i].opcode && op)
+	{
+		if (strcmp(op, ops[i].opcode) == 0)
+		{
+			
+				data->str = str ? str : "e";
+				ops[i].f(data->stack, data->line_number);
+				return;
+		}
+		i++;
+	}
+	fprintf(stderr, "L%u: unknown instruction %s\n", data->line_number, op);
+	/*free_stack(data->stack);*/
+	free(data->line);
+	fclose(data->file);
+	exit(EXIT_FAILURE);
+}
---
+++ monty.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+Data global_data = {NULL, NULL, 0, NULL ,NULL};
+/**
+ * 
+*/
+int main(int argc, char **argv)
+{
+    stack_t *stack = NULL;
+	char *line = NULL;
+	size_t len = 0;
+	unsigned int line_number = 1;
+	FILE *file;
+
+    if (argc != 2)
+    {
+        fprintf(stderr,  "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+    file = fopen(argv[1], "rb");
+    if (file == NULL)
+    {
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+    }
+    global_data.file = file;
+    global_data.stack = &stack;
+    while (getline(&line, &len, file) != -1)
+    {
+        global_data.line = line;
+        global_data.line_number = line_number++;
+        execute(&global_data);
+    }
+    free(line);
+    fclose(file);
+    /*free_stack(&stack);*/
+    return (0);
+}
